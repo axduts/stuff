d83f42b315acd333895b0d5b3f149cb234696c87
Alex Duts <duts@MacBook-Pro.local>
2016-12-18 14:16:09 +0200

    Draft data structure

--- perl/lib/Term/Dialog.pm
+++ perl/lib/Term/Dialog.pm
@@ -13,4 +13,54 @@ sub new {
     return $self;
 }
 
+sub run {
+    my $self = shift;
+    
+    my $config = $self->{config};
+    
+    for my $unit ( @{ $config } ) {
+        
+        if (! defined $unit->{id}) {
+            die "[ERROR]: id of the dialog unit must be presented\n";
+        }
+        
+        if (! defined $unit->{rendering}) {
+            # Default is printing from "echo" directly
+            $unit->{rendering} = sub {
+                return ();
+            }
+        }
+        
+        if (! defined $unit->{input}) {
+            # Default is passing undef
+            $unit->{input} = sub {return undef};
+        }
+        
+        if (! defined $unit->{commit}) {
+            # Default is do nothing
+            $unit->{commit} = sub {return};
+        }
+        
+        if (! defined $unit->{next}) {
+            # Default is do nothing
+            $unit->{next} = sub {return};
+        }
+        
+        # Resolve "echo" macros
+        my @rvars = $unit->{rendering}->();
+        my $resolved_echo = resolve_macros(
+            string  => $unit->{echo},
+            vars    => \@rvars
+        );
+        
+        
+    }
+}
+
+sub resolve_macros {
+    my $args = {@_};
+    
+
+}
+
 1;
--- test.pl
+++ test.pl
@@ -5,12 +5,41 @@ use Term::Dialog;
 
 use Data::Dumper;
 
+my $data = {
+};
+
 my $d = Term::Dialog->new(config => [
     {
         id          => 'animals_names_prompt',
         echo        => '- - - - - - - - Animals names - - - - - - - - -',
-        rendering   => sub { return 1; },
-        input       => sub { return; },
-        commit      => sub { return; }
+    },
+    {
+        id => 'animals_names_question',
+        echo => 'Enter animal name %{order}',
+        rendering => sub {
+            my $args = {@_};
+            
+            my $order = defined $data->{animals} ?
+                        scalar @{ $data->{animals} } + 1 :
+                        0;
+            
+            if (scalar @{ $data->{animals} } < 3 ) {
+                return (order => $order);
+            }
+            return undef;
+        },
+        input => sub {
+            return shift;
+        },
+        commit => sub {
+            my $args = {@_};
+            push @{ $data->{animals} }, $args->{value};
+        },
+        next => sub {
+            my $args = {@_};
+            return $args->{parent};
+        }
     }
 ]);
+
+$d->run;
